23a92741ea7e47d0d212f407980fca53be105de5
ruios <ruiosli@kreditme.id>
2019-10-09 19:53:50 +0800

    try to implents retry

--- index.js
+++ index.js
@@ -8,13 +8,15 @@ const PluginError = require('gulp-util').PluginError;
 
 const PLUGIN_NAME = 'gulp-ali-oss-upload';
 
-async function main(opts) {
+function main(opts) {
 
-    if(!opts.ossConfig){
+    if (!opts.ossConfig) {
         throw new PluginError(PLUGIN_NAME, 'ossConfig is required');
     }
 
     let failList = [];
+    let count = 0;
+    let retryTimes = 0;
 
     const oss = new OSS({
         accessKeyId: opts.ossConfig.accessKeyId,
@@ -22,11 +24,6 @@ async function main(opts) {
         endpoint: opts.ossConfig.endpoint,
         bucket: opts.ossConfig.bucket
     });
-    const headers = {
-        'Cache-Control': 'max-age=' + 3600 * 1,
-        mime: mime.getType(uploadPath),
-        ...(opts.headers ? opts.headers : {})
-    };
 
     // if (opts.clean) {
     //     // 删除 指定的 oss 文件 或目录
@@ -38,12 +35,18 @@ async function main(opts) {
     //     }
     // }
 
-    return through.obj(function (file, enc, next) {
+
+    const stream = through.obj(function (file, enc, next) {
         if (!file.isBuffer()) {
             next();
             return;
         }
         const uploadPath = path.relative(file.base, file.path);
+        const headers = {
+            'Cache-Control': 'max-age=' + 3600 * 1,
+            mime: mime.getType(uploadPath),
+            ...(opts.headers ? opts.headers : {})
+        };
         oss
             .put(uploadPath, file.contents, {headers})
             .then(function (res) {
@@ -56,7 +59,30 @@ async function main(opts) {
                 log(err);
                 next();
             });
-    })
+    });
+
+    stream.on('end', function () {
+        if (failList.length) {
+            log(
+                chalk.red('Deploy oss files failed:'),
+                '\n' + failList.join('\n')
+            );
+            // retry 3 times at most
+            if (retryTimes < 3) {
+                retryTimes++;
+                log(chalk.red('Retry deploy oss time ' + retryTimes + '...'));
+                // TODO implents retry
+            } else {
+                log(chalk.red('Deploy oss failed!'));
+            }
+        }else{
+            log(chalk.green('Deploy oss success!'));
+        }
+    });
+
+
+    return stream;
+
 }
 
 module.exports = main;
